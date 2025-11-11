027ac2e12e20bae2ebca5ec63248f3cb78346647
mirajamous <mirajamous100@gmail.com>
2025-11-11 11:11:13 +0200

    Initial commit - Form validation with jQuery

---
+++ index.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    $("#loginForm").validate({
+       rules:{
+         email:{
+           required: true,
+           email: true,
+         },
+         password:{
+           required: true,
+           minlength: 8,
+           uppercase: true,
+           lowercase: true,
+           digit: true,
+           specialChar: true
+         },
+       },messages: {
+        email:{
+            required: "Please enter your email",
+            email: "Please enter a valid email address"
+         },
+         password:{
+           required: "Please enter your password",
+           minlength: "Password must be 8 characters at least",
+           digit: "Please enter one digit at least",    
+           uppercase: "Please enter one capital letter at least",
+           lowercase: "Please enter one small letter at least",
+           specialChar: "Please enter one special character at least"
+           
+         }
+         
+         
+         
+      },
+      
+      submitHandler: function(form) {
+    
+        alert("Form submitted successfully!");
+        $(form).submit();
+      }
+     });
+      
+    
+    
+     });
